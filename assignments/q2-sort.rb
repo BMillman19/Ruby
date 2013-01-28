0359ec6aabbc6500e25780b87133e898f2d79dde
BMillman19 <brandon.millman@gmail.com>
2013-01-27 20:18:30 -0500

    Solution to sort, quicksort for implementation

--- assignments/q2-sort.rb
+++ assignments/q2-sort.rb
@@ -1,3 +1,24 @@
 def sort(arr)
-  # ...
+	return arr if arr.length <= 1
+	pivotIndex = rand(arr.length)
+	left, middle, right = split(pivotIndex, arr)
+	return sort(left) + middle + sort(right)
+end
+
+def split(index, arr)
+	pivot = arr[index]
+	left = Array.[]
+	right = Array.[]
+	middle = Array.[]
+	arr.each { |x|
+		case pivot <=> x
+			when 0
+				middle.push x
+			when 1
+				left.push x
+			else
+				right.push x
+			end
+	}
+	return left, middle, right
 end
