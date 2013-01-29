aea20c7de9204dd7ea33b2bb63d4ce5992605cb5
BMillman19 <brandon.millman@gmail.com>
2013-01-29 16:04:06 -0500

    Solution to bonus

--- assignments/bonus-make-change.rb
+++ assignments/bonus-make-change.rb
@@ -1,3 +1,17 @@
 def make_change(amount, coins = [25, 10, 5, 1])
-  # ...
+	results = Array.[]
+	results[0] = Array.[]
+
+	for i in 1..amount
+  		results[i] = 
+  			coins.
+  			map { |coin| 
+  				Array.new(results[i - coin]).push(coin) if coin <= i }.
+  			select { |list|  
+  				list.is_a? Array }.
+  			min { |a, b| 
+  				a.length <=> b.length }
+  	end
+
+  	return results[amount]
 end
